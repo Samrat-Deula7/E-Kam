03301390b55660ed95ee945f8ecbe0570c209d4c
Samrat-Deula7 <samratdeula461@gmail.com>
2025-10-26 18:12:38 +0545

    finished cards

---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,34 @@
+const Card = () => {
+  return (
+    <div className="flex flex-col items-center  bg-stone-600 h-80 w-80 rounded-2xl px-4 py-3">
+      {/* This is div 1 */}
+      <div className="grid grid-cols-2  mb-3">
+        <div className="w-23 h-23 md:w-28 md:h-28 lg:w-31 lg:h-31  rounded-full bg-linear-to-r from-stone-600 to-white ">
+          <div className="w-full h-full flex justify-center items-center ">
+            <img
+              src=""
+              alt=""
+              className="w-20 h-20 md:w-25 md:h-25 lg:w-28 lg:h-28 bg-blue-600   bg-cover bg-center rounded-full overflow-clip"
+            />
+          </div>
+        </div>
+        <div>
+          <h3>Samrat Deula</h3>
+          <h3>Phone no: 98432442</h3>
+          <p>Email: abc@gmail.com</p>
+        </div>
+      </div>
+      {/* This is div 2 */}
+      <div className="bg-[#181C14] rounded-full p-2 w-auto mb-3">
+        <h3 className="text-gray-400 ">Plumber</h3>
+      </div>
+      {/* This is div 3 */}
+      <div className="text-gray-400">
+        <h3>Experience: 3 years</h3>
+        <h3>Cost: 1000 - 4000</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- frontend/src/components/services.jsx
+++ frontend/src/components/services.jsx
@@ -1,4 +1,25 @@
+import Card from "./Card";
+
 const Services=()=>{
-return <div className="pt-16 min-h-screen">this is services page</div>;
+return (
+  <div className="pt-10 min-h-screen px-8 flex flex-col items-center justify-center w-full">
+    <h1 className="font-bold bg-linear-to-r from-stone-600 to-white bg-clip-text text-transparent text-2xl mb-5 md:text-6xl md:mb-7">
+      Search for the service you want
+    </h1>
+
+    <div className="flex items-center">
+      <input
+        type="text"
+        required
+        className="w-full h-12 bg-white/5 border border-white/10 border-r-0 rounded-l-full px-4 py-3 text-white transition focus:outline-none focus:border-gray-600 focus:bg-stone-400/5"
+        placeholder="Search"
+      />
+      <i class="fa-solid fa-magnifying-glass h-12 bg-white/5 border  border-white/10 border-l-0 rounded-r-full  px-8 py-3 "></i>
+    </div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  place-items-center mt-6 mx-6 my-3">
+      <Card />
+    </div>
+  </div>
+);
 }
 export default Services;
