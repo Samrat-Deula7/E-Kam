9a4dd403dd87af1db568e1c784651a70a7582bf5
Samrat-Deula7 <samratdeula461@gmail.com>
2025-10-31 16:51:26 +0545

    just adding some comments

--- frontend/src/ProtectedRoute.jsx
+++ frontend/src/ProtectedRoute.jsx
@@ -1,6 +1,9 @@
 import { Navigate } from "react-router-dom";
 const ProtectedRoute=({children})=>{
     const token=localStorage.getItem("token");
+
+    // The logic behind the following code is if no token take to="/" else just render children which is <Admin/> in this case
+    
     if(!token){
         return(<Navigate to="/" replace/>)
     }
