a5a24b4376852b6cce9f6adf3eb3e3ba65b3eaef
Samrat-Deula7 <samratdeula461@gmail.com>
2025-11-08 12:30:18 +0545

    Connecting to cloud mongodb

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,8 @@
 const mongoose =require("mongoose");
-const mongoURL =
-  "mongodb://atlas-sql-690ee33f94f443685364a88c-uo94c4.a.query.mongodb.net/sample_mflix?ssl=true&authSource=admin";
+const CloudMongoDB = process.env.CloudMongoDB;
+
+const mongoURL = CloudMongoDB;
+  
 
 const connectToMongo=()=>{
     try {
