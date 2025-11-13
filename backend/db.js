231fc2d5983ca3996b03f0dcec6470aacef19cb9
Samrat-Deula7 <samratdeula461@gmail.com>
2025-11-13 17:28:28 +0545

    making connectToMongo asynchronous

--- backend/db.js
+++ backend/db.js
@@ -6,13 +6,13 @@ const CloudMongoDB = process.env.CloudMongoDB;
 const mongoURL = CloudMongoDB;
   
 
-const connectToMongo=()=>{
-    try {
-        mongoose.connect(mongoURL);
-        console.log("✅ Connected to Mongoose successfully");
-    } catch (error) {
-            console.error("❌ Mongoose connection failed:", error);
-    }
-}
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(mongoURL);
+    console.log("✅ Connected to Mongoose successfully");
+  } catch (error) {
+    console.error("❌ Mongoose connection failed:", error);
+  }
+};
 
 module.exports=connectToMongo;
