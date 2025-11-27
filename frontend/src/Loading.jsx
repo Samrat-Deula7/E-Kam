c1027dc3eea60f1576f82efba08b29bc77cb8a16
Samrat-Deula7 <samratdeula461@gmail.com>
2025-11-27 16:39:24 +0545

    Finished the loading component

--- frontend/src/Loading.jsx
+++ frontend/src/Loading.jsx
@@ -2,16 +2,18 @@ import loadingUrl from "./assets/loading.gif"
 const Loading = ({ loading }) => {
   return (
     <div
-      className={`fixed top-0 left-0 max-w-screen w-full bg-[rgba(10,10,10,0.8)] z-40 flex flex-col items-center justify-center transition-all duration-300 ease-in-out ${
-        loading
-          ? "h-screen opacity-100 pointer-events-auto"
-          : "h-0 opacity-0 pointer-events-none"
+      className={`fixed top-0 left-0 max-w-screen w-full bg-[rgba(10,10,10,0.8)] z-40 flex flex-col items-center justify-center   ${
+        loading ? "h-screen  pointer-events-auto" : "h-0  pointer-events-none"
       }`}
     >
       <img
         src={loadingUrl}
         alt="loading"
-        className="rounded-full w-10 h-10 md:w-15 md:h-15"
+        className={`rounded-full ${
+          loading
+            ? "  pointer-events-auto w-10 h-10 md:w-15 md:h-15"
+            : "h-0  pointer-events-none"
+        }`}
       />
     </div>
   );
